08ae62addece10cccd074dbf863286f79a868927
Shriti Gupta <shriti@Shritis-MacBook-Pro.local>
2024-11-28 17:43:39 +0530

    Reduce number of parameters

--- model/mnist_model.py
+++ model/mnist_model.py
@@ -5,36 +5,33 @@ class MNISTModel(nn.Module):
     def __init__(self):
         super(MNISTModel, self).__init__()
         
-        # Simpler architecture with focused capacity
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
-        self.bn1 = nn.BatchNorm2d(32)
+        # Reduced number of filters
+        self.conv1 = nn.Conv2d(1, 8, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(8)
         
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
-        self.bn2 = nn.BatchNorm2d(64)
+        self.conv2 = nn.Conv2d(8, 16, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(16)
         
-        # Adjusted FC layers for better capacity
-        self.fc1 = nn.Linear(64 * 7 * 7, 128)
-        self.fc2 = nn.Linear(128, 10)
+        # Significantly reduced FC layer size
+        self.fc1 = nn.Linear(16 * 7 * 7, 64)
+        self.fc2 = nn.Linear(64, 10)
         
         self.pool = nn.MaxPool2d(2, 2)
-        self.dropout = nn.Dropout(0.1)  # Reduced dropout
+        self.dropout = nn.Dropout(0.1)
         self.relu = nn.ReLU()
         
     def forward(self, x):
-        # First block
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)
         x = self.pool(x)
         
-        # Second block
         x = self.conv2(x)
         x = self.bn2(x)
         x = self.relu(x)
         x = self.pool(x)
         
-        # Flatten and FC layers
-        x = x.view(-1, 64 * 7 * 7)
+        x = x.view(-1, 16 * 7 * 7)
         x = self.dropout(x)
         x = self.relu(self.fc1(x))
         x = self.fc2(x)
